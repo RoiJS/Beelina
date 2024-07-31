095d42acfaa818d8cdb3ba79179479093f277c54
Roam <Roi.Amatong@qlogic.se>
2024-07-31 16:25:15 +0200

    #229-Remove unused number formatter function

--- Beelina.APP/src/app/_helpers/formatters/number-formatter.helper.ts
+++ Beelina.APP/src/app/_helpers/formatters/number-formatter.helper.ts
@@ -7,13 +7,6 @@ export class NumberFormatter {
       .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
   }
 
-  static formatCurrencyWithoutSymbol(value: number) {
-    return `${value
-      .toFixed(2)
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
-  }
-
   static roundToDecimalPlaces(num: number, decimalPlaces: number) {
     const factor = Math.pow(10, decimalPlaces);
     return Math.round(num * factor) / factor;
