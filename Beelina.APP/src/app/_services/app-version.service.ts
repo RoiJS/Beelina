4b11ff0bbbd4dcae1a02babb86e71dbd583442e4
Roam <Roi.Amatong@qlogic.se>
2024-09-13 15:03:15 +0200

    Adjustments on register payment button options

--- Beelina.APP/src/app/_services/app-version.service.ts
+++ Beelina.APP/src/app/_services/app-version.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class AppVersionService {
-  private _appVersion = new BehaviorSubject<string>('1.23.02');
+  private _appVersion = new BehaviorSubject<string>('1.23.03');
 
   constructor(private translate: TranslateService) { }
 
