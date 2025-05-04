262f5e13e8996b0cdbaecaf416e200dd40498294
Roam <Roi.Amatong@qlogic.se>
2025-05-04 19:16:26 +0200

    Upgraded to version 1.25.4

--- Beelina.APP/src/app/_services/app-version.service.ts
+++ Beelina.APP/src/app/_services/app-version.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class AppVersionService {
-  private _appVersion = new BehaviorSubject<string>('1.25.3');
+  private _appVersion = new BehaviorSubject<string>('1.25.4');
 
   constructor(private translate: TranslateService) { }
 
