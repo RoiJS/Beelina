b0fe6b0266ff4649e7de98226691f9fbda505f67
Roam <Roi.Amatong@qlogic.se>
2025-06-24 07:11:29 +0200

    Added new unit test case for testing

--- Beelina.UnitTest/UnitTest1.cs
+++ Beelina.UnitTest/UnitTest1.cs
@@ -17,4 +17,19 @@ public class UnitTest1
         // Assert
         Assert.Equal(result, expectedResult);
     }
+
+    [Theory]
+    [InlineData(2, 1, 1)]
+    public void Test2(int num1, int num2, int expectedResult)
+    {
+        // Arrange
+        var varNum1 = num1;
+        var varNum2 = num2;
+
+        // Act
+        var result = varNum1 - varNum2;
+
+        // Assert
+        Assert.Equal(result, expectedResult);
+    }
 }
