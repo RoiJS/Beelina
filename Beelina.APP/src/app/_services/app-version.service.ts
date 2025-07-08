84410526399beb25ffb8b080f5bbd9e7f1f297bc
Roam <Roi.Amatong@qlogic.se>
2025-07-08 19:01:14 +0200

    Upgrade app version to 1.29.0 and update copyright year to 2025 in translations

--- Beelina.APP/src/app/_services/app-version.service.ts
+++ Beelina.APP/src/app/_services/app-version.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class AppVersionService {
-  private _appVersion = new BehaviorSubject<string>('1.28.08');
+  private _appVersion = new BehaviorSubject<string>('1.29.0');
 
   private translate = inject(TranslateService);
 
