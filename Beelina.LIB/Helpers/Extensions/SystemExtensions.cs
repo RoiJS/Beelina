3e0ac693d7b8b56732a9dfb47032f7e0e6b40d82
Roam <Roi.Amatong@qlogic.se>
2025-07-25 18:09:50 +0200

    Update ConvertToTimeZone method to handle UTC dateTime correctly

--- Beelina.LIB/Helpers/Extensions/SystemExtensions.cs
+++ Beelina.LIB/Helpers/Extensions/SystemExtensions.cs
@@ -75,7 +75,9 @@ namespace Beelina.LIB.Helpers.Extensions
         public static DateTime ConvertToTimeZone(this DateTime dateTime, string timezoneId)
         {
             var timeZone = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
-            var utcDateTime = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+            var utcDateTime = dateTime.Kind == DateTimeKind.Utc
+                ? dateTime
+                : dateTime.ToUniversalTime();
             return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, timeZone);
         }
 
