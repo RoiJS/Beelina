02c79727db0dfa0ed5db3df28c21fa7110ad5b21
Roam <Roi.Amatong@qlogic.se>
2025-08-12 19:10:52 +0200

    Add migrations to update report availability and control settings
    - Created migration to make the Customer Detailed Transactions Report available to all business models by updating the OnlyAvailableOnBusinessModel field in the Reports table.
    - Added migration to modify the Sales Agent Dropdown Report Control availability by updating the OnlyAvailableOnBusinessModel and OnlyAvailableOnBusinessModelForMinimumPrivilege fields in the ReportControlsRelations table.

---
+++ Beelina.LIB/Migrations/BeelinaData/20250812170012_MakeCustomerDetailedTransactionsReportAvailableToAllBusinessModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations.BeelinaData
+{
+    /// <inheritdoc />
+    public partial class MakeCustomerDetailedTransactionsReportAvailableToAllBusinessModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE Reports set OnlyAvailableOnBusinessModel = NULL where Id in (8)
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE Reports set OnlyAvailableOnBusinessModel = 2 where Id in (8)
+            ");
+        }
+    }
+}
---
+++ Beelina.LIB/Migrations/BeelinaData/20250812170449_MakeCustomerDetailedTransactionsReportSalesAgentDropdownReportControlAvailable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations.BeelinaData
+{
+    /// <inheritdoc />
+    public partial class MakeCustomerDetailedTransactionsReportSalesAgentDropdownReportControlAvailable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ReportControlsRelations SET OnlyAvailableOnBusinessModel	= '1;3', OnlyAvailableOnBusinessModelForMinimumPrivilege = 2  WHERE ReportId = 8 AND ReportControlId = 5;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ReportControlsRelations SET OnlyAvailableOnBusinessModel	= NULL, OnlyAvailableOnBusinessModelForMinimumPrivilege = NULL  WHERE ReportId = 8 AND ReportControlId = 5;
+            ");
+        }
+    }
+}
