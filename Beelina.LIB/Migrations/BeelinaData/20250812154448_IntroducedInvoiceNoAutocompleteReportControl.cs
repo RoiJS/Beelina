58666243fa5be6641f7ffbd200f71275ed374db5
Roam <Roi.Amatong@qlogic.se>
2025-08-12 18:28:43 +0200

    #331-Add migrations for report control relations and modify order for customer detailed transactions report
    - Introduced a new migration to insert a relation for the Invoice No Autocomplete Report Control.
    - Added a migration to modify the order of a report control in the Customer Detailed Transactions Report.

---
+++ Beelina.LIB/Migrations/BeelinaData/20250812154448_IntroducedInvoiceNoAutocompleteReportControl.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations.BeelinaData
+{
+    /// <inheritdoc />
+    public partial class IntroducedInvoiceNoAutocompleteReportControl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                        @"
+SET IDENTITY_INSERT ReportControls ON
+INSERT INTO ReportControls(
+    Id
+    , Name	
+    , ParentContainerName	
+    , CustomCSSClasses	
+    , IsActive	
+    , IsDelete	
+    , DateCreated	
+    , DateUpdated	
+    , DateDeleted	
+    , DateDeactivated
+    ,LabelIdentifier
+)
+SELECT 
+	8
+	,'InvoiceNoAutocomplete'
+	, 'mainPageContainer'
+	, ''
+	, 1
+	, 0
+	, GETDATE()
+	, '0001-01-01 00:00:00.0000000'
+	, '0001-01-01 00:00:00.0000000'
+	, '0001-01-01 00:00:00.0000000'
+	, 'REPORT_DETAILS_PAGE.CONTROL_INFORMATION.INVOICE_NO_AUTOCOMPLETE_CONTROL.LABEL'
+
+SET IDENTITY_INSERT ReportControls OFF
+                        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM ReportControls WHERE Id = 8;");
+        }
+    }
+}
---
+++ Beelina.LIB/Migrations/BeelinaData/20250812155154_IntroducedInvoiceNoAutocompleteReportControlReportControlRelations.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations.BeelinaData
+{
+    /// <inheritdoc />
+    public partial class IntroducedInvoiceNoAutocompleteReportControlReportControlRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+INSERT INTO ReportControlsRelations (
+	ReportId
+	, ReportControlId
+	, [Order]
+	, DefaultValue
+	, IsActive
+	, IsDelete
+	, DateCreated
+	, DateUpdated
+	, DateDeleted
+	, DateDeactivated
+)
+SELECT 
+	8
+	, 8
+	, 1
+	, ''
+	, 1
+	, 0
+	, GETDATE()
+	, '0001-01-01 00:00:00.0000000'
+	, '0001-01-01 00:00:00.0000000'
+	, '0001-01-01 00:00:00.0000000'
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM ReportControlsRelations WHERE ReportId = 8 AND ReportControlId = 8;");
+        }
+    }
+}
