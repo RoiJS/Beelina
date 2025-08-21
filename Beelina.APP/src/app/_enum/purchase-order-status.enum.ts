95b27de21c02000813648ada100493c69ec41d10
Roam <Roi.Amatong@qlogic.se>
2025-08-21 17:19:22 +0200

    fix: Correct return values in PurchaseOrderStatusEnum functions for consistency

--- Beelina.APP/src/app/_enum/purchase-order-status.enum.ts
+++ Beelina.APP/src/app/_enum/purchase-order-status.enum.ts
@@ -6,19 +6,19 @@ export enum PurchaseOrderStatusEnum {
 export function getPurchaseOrderStatusEnum(value: PurchaseOrderStatusEnum): number {
   switch (value) {
     case PurchaseOrderStatusEnum.OPEN:
-      return 1;
+      return 0;
     case PurchaseOrderStatusEnum.CLOSED:
-      return 2;
-    default:
       return 1;
+    default:
+      return 0;
   }
 }
 
 export function getPurchaseOrderStatusFromNumber(value: number): PurchaseOrderStatusEnum {
   switch (value) {
-    case 1:
+    case 0:
       return PurchaseOrderStatusEnum.OPEN;
-    case 2:
+    case 1:
       return PurchaseOrderStatusEnum.CLOSED;
     default:
       return PurchaseOrderStatusEnum.OPEN;
