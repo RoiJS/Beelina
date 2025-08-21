3f9d5b69c27fdd345d1ddd30138b41badf7ae2d8
Roam <Roi.Amatong@qlogic.se>
2025-08-21 09:08:47 +0200

    #342-Introduce new db migrations regarding adding order by product name on the Report SPs

---
+++ Beelina.LIB/Migrations/20250821064110_202508210826_AlterWarehouseEndingInventoryReportModel1.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202508210826_AlterWarehouseEndingInventoryReportModel1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.Sql(@"
+-- =============================================
+-- Author:		Amatong, Roi Larrence
+-- Create date: 2024-08-12
+-- Description:	Ending inventory data from warehouse for Admin accounts
+-- =============================================
+-- exec Report_Ending_Inventory_Per_Product_Warehouse_Model_1 @startDate='2024-01-01', @endDate = '2024-08-30'
+ALTER   PROCEDURE [dbo].[Report_Ending_Inventory_Per_Product_Warehouse_Model_1]
+	@startDate VARCHAR(10) = NULL
+	, @endDate VARCHAR(10) = NULL
+	, @userId INT = 0
+	, @warehouseId INT = 1
+AS
+BEGIN
+
+	;WITH PreviousStocksPerProduct AS (
+		SELECT 
+			ot.ProductId
+			, ot.Code
+			, ot.[Name]
+			, SUM(ot.AbsoluteStocks) AS AbsoluteStocks
+		FROM
+			dbo.TVF_Product_Absolute_Stocks(0, @warehouseId) ot
+		WHERE
+			StockDate < CONVERT(DATE, @startDate) 
+			AND ot.StockAuditSource = 1 -- Withdrawal
+		GROUP BY
+			ot.ProductId
+			, ot.Code
+			, ot.[Name]
+	)
+
+	, PreviousAbsoluteWarehouseStocksPerProduct AS (
+		SELECT 
+			pas.ProductId
+			, pas.Code
+			, pas.[Name]
+			, pas.PricePerUnit
+			, SUM(pas.AbsoluteStocks) AS Stock
+		FROM 
+			dbo.TVF_Warehouse_Product_Absolute_Stocks(@warehouseId) pas
+		WHERE 
+			StockDate < CONVERT(DATE, @startDate)
+		GROUP BY 
+			pas.ProductId
+			, pas.Code
+			, pas.[Name]
+			, pas.PricePerUnit
+	)
+
+	, PreviousRemainingWarehouseStockPerProduct AS (
+	
+		SELECT 
+			pp.ProductId
+			, pp.Code
+			, pp.[Name]
+			, pp.PricePerUnit
+			, (COALESCE(pp.Stock, 0) - COALESCE(pt.AbsoluteStocks, 0)) AS RemainingStock
+		FROM 
+			PreviousAbsoluteWarehouseStocksPerProduct pp
+
+			LEFT JOIN PreviousStocksPerProduct pt
+			ON pt.ProductId = pp.ProductId
+	)
+
+	, CurrentStocksPerProduct AS (
+		SELECT 
+			ot.ProductId
+			, ot.Code
+			, ot.[Name]
+			, SUM(ot.AbsoluteStocks) AS AbsoluteStocks
+		FROM
+			dbo.TVF_Product_Absolute_Stocks(0, @warehouseId) ot
+		WHERE
+			StockDate BETWEEN @startDate AND @endDate
+			AND ot.StockAuditSource = 1 -- Withdrawal
+		GROUP BY
+			ot.ProductId
+			, ot.Code
+			, ot.[Name]
+	)
+
+	, CurrentAbsoluteWarehouseStocksPerProduct AS (
+		SELECT 
+			pas.ProductId
+			, pas.Code
+			, pas.[Name]
+			, pas.PricePerUnit
+			, SUM(pas.AbsoluteStocks) AS Stock
+		FROM 
+			dbo.TVF_Warehouse_Product_Absolute_Stocks(@warehouseId) pas
+		WHERE 
+			StockDate BETWEEN @startDate AND @endDate
+		GROUP BY 
+			pas.ProductId
+			, pas.Code
+			, pas.[Name]
+			, pas.PricePerUnit
+	)
+
+	, CurrentRemainingStockPerWarehouseProduct AS (
+	
+		SELECT 
+			pt.ProductId
+			, pt.Code
+			, pt.[Name]
+			, pp.PricePerUnit
+			, (pp.Stock - pt.AbsoluteStocks) AS RemainingStock
+		FROM 
+			CurrentStocksPerProduct pt
+
+			LEFT JOIN CurrentAbsoluteWarehouseStocksPerProduct pp
+			ON pt.ProductId = pp.ProductId
+	)
+
+	, EndingStocksPerWarehouseProductReport AS (
+
+		SELECT
+			p.Id
+			, p.Code
+			, p.[Name]
+			, pu.[Name] AS UnitName
+			, (COALESCE(pp.RemainingStock, 0) + COALESCE(c.Stock, 0)) AS BeginningStocks
+			, (COALESCE(pp.RemainingStock, 0) + COALESCE(c.Stock, 0)) * COALESCE(pw.PricePerUnit, 0) AS BeginningStocksValue
+			, (COALESCE(pp.RemainingStock, 0) + COALESCE(c.Stock, 0) - COALESCE(cp.AbsoluteStocks, 0)) AS EndingStocks
+			, (COALESCE(pp.RemainingStock, 0) + COALESCE(c.Stock, 0) - COALESCE(cp.AbsoluteStocks, 0)) * COALESCE(pw.PricePerUnit, 0) AS EndingStocksValue
+			--, pp.PricePerUnit
+			--, pp.RemainingStock
+			--, c.Stock
+			--, cp.SoldQuantity
+			--, ppp.Stock AS PreviousAbsoluteStock
+		FROM
+
+			Products p
+
+			LEFT JOIN ProductStockPerWarehouse pw
+			ON p.Id = pw.ProductId
+
+			LEFT JOIN CurrentAbsoluteWarehouseStocksPerProduct c
+			ON p.Id = c.ProductId 
+
+			LEFT JOIN PreviousRemainingWarehouseStockPerProduct pp
+			ON pp.ProductId =  p.Id
+
+			LEFT JOIN CurrentStocksPerProduct cp
+			ON cp.ProductId =  p.Id
+
+			LEFT JOIN PreviousAbsoluteWarehouseStocksPerProduct ppp
+			ON ppp.ProductId =  p.Id
+
+			LEFT JOIN ProductUnits pu
+			ON pu.Id = p.ProductUnitId
+	)
+
+	SELECT 
+		ep.Code
+		, CONCAT(ep.[Name],' : ',ep.UnitName) AS [Name]
+		, ep.BeginningStocks
+		, CAST(ep.BeginningStocksValue AS DECIMAL(10, 2)) AS BeginningStocksValue
+		, ep.EndingStocks
+		, CAST(ep.EndingStocksValue AS DECIMAL(10, 2)) AS EndingStocksValue
+		, (ep.BeginningStocks - ep.EndingStocks) AS WithdrawnStocks
+		, (CAST(ep.BeginningStocksValue AS DECIMAL(10, 2)) - CAST(ep.EndingStocksValue AS DECIMAL(10, 2))) AS WithdrawnStocksValue
+	FROM 
+		EndingStocksPerWarehouseProductReport ep
+	WHERE
+		ep.BeginningStocks <> 0 and ep.EndingStocks <> 0
+	ORDER BY
+		ep.[Name]
+END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Beelina.LIB/Migrations/20250821065218_202508210826_AlterProductWithdrawalReportVersion1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Beelina.LIB.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202508210826_AlterProductWithdrawalReportVersion1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+migrationBuilder.Sql(@"
+-- =============================================
+-- Author:		Roi Larrence Amatong
+-- Create date: 2023-12-05
+-- Description:	Product Withdrawal Report per sales agent
+
+-- Modified By: Roi Larrence Amatong
+-- Modified Date: 2023-12-11
+-- Description: Make sure to only get info from Withdrawal action
+
+-- Modified By: Roi Larrence Amatong
+-- Modified Date: 2025-03-26
+-- Description: Joined table ProductWithdrawalEntries to get withdrawal slip no
+-- =============================================
+-- exec Report_Product_Withdrawal @startDate = '2025-03-03', @endDate = '2025-03-31', @salesAgentId = 3
+
+ALTER PROCEDURE [dbo].[Report_Product_Withdrawal] 
+    @startDate VARCHAR(10) = NULL
+    , @endDate VARCHAR(10) = NULL
+    , @salesAgentId INT = 0
+    , @userId INT = 0
+AS
+BEGIN
+
+    DECLARE @salesAgentName AS NVARCHAR(MAX)
+    SET @salesAgentName = (SELECT CONCAT(u.FirstName, ' ', u.LastName) FROM UserAccounts u WHERE Id = @salesAgentId)
+
+    -- (1) Report Header
+    SELECT
+        @salesAgentName AS SalesAgentName
+        , @startDate AS FromDate
+        , @endDate AS ToDate
+                    
+    -- (2) Report Table results
+    SELECT
+        COALESCE(ps.WithdrawalSlipNo, COALESCE(pw.WithdrawalSlipNo, '')) AS WithdrawalSlipNo
+        , p.Code AS ProductCode
+        , p.Name AS ProductName
+        , pu.Name AS ProductUnit
+        , SUM(ps.Quantity) AS Quantity
+    FROM 
+
+        Products p
+
+        LEFT JOIN ProductStockPerPanels pp
+        ON p.Id = pp.ProductId
+
+        LEFT JOIN ProductStockAudits ps
+        ON pp.Id = ps.ProductStockPerPanelId
+
+        LEFT JOIN ProductWithdrawalEntries pw
+        ON pw.Id = ps.ProductWithdrawalEntryId
+
+        LEFT JOIN ProductUnits pu
+        ON p.ProductUnitId = pu.Id
+
+    WHERE
+        pp.UserAccountId = @salesAgentId
+        AND p.IsActive = 1
+        AND p.IsDelete = 0
+        AND ps.IsActive = 1
+        AND ps.IsDelete = 0
+        AND ps.StockAuditSource = 1 -- From Withdrawal
+        AND FORMAT(pw.StockEntryDate, 'yyyy-MM-dd') BETWEEN CONVERT(DATE, @startDate) AND CONVERT(DATE, @endDate)
+
+    GROUP BY
+        ps.WithdrawalSlipNo 
+        , pw.WithdrawalSlipNo
+        , p.Code
+        , p.Name
+        , pu.Name
+
+	ORDER BY
+		p.[Name]
+END
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
