319ca6db8a838ec9f0a028921f7fbfab609105f4
Roam <Roi.Amatong@qlogic.se>
2025-08-25 02:56:12 +0200

    refactor: Simplify SQL insertion in RegisterPurchaseOrderReport migration by removing error handling

--- Beelina.LIB/Migrations/BeelinaData/20250820065801_RegisterPurchaseOrderReport.cs
+++ Beelina.LIB/Migrations/BeelinaData/20250820065801_RegisterPurchaseOrderReport.cs
@@ -11,51 +11,43 @@ namespace Beelina.LIB.Migrations.BeelinaData
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-BEGIN TRY
-    IF NOT EXISTS (SELECT 1 FROM [Reports] WHERE [Id] = 12 OR [ReportClass] = 'PurchaseOrderReport')
-    BEGIN
-        INSERT INTO [Reports] (
-            [Id],
-            [NameTextIdentifier],
-            [DescriptionTextIdentifier],
-            [ReportClass],
-            [Custom],
-            [StoredProcedureName],
-            [ModuleId],
-            [UserMinimumModulePermission],
-            [IsActive],
-            [IsDelete],
-            [DateCreated],
-            [DateUpdated],
-            [DateDeleted],
-            [DateDeactivated],
-            [UserMaximumModulePermission],
-            [OnlyAvailableOnBusinessModel],
-            [Lock]
-        ) VALUES (
-            12,
-            'PURCHASE_ORDER_REPORT_NAME',
-            'PURCHASE_ORDER_REPORT_DESC',
-            'PurchaseOrderReport',
-            0,
-            'Report_Purchase_Orders',
-            1,
-            2,
-            1,
-            0,
-            GETDATE(),
-            '0001-01-01 00:00:00.0000000',
-            '0001-01-01 00:00:00.0000000',
-            '0001-01-01 00:00:00.0000000',
-            3,
-            NULL,
-            1
-        );
-    END
-END TRY
-BEGIN CATCH
-    THROW;
-END CATCH
+INSERT INTO [Reports] (
+    [Id],
+    [NameTextIdentifier],
+    [DescriptionTextIdentifier],
+    [ReportClass],
+    [Custom],
+    [StoredProcedureName],
+    [ModuleId],
+    [UserMinimumModulePermission],
+    [IsActive],
+    [IsDelete],
+    [DateCreated],
+    [DateUpdated],
+    [DateDeleted],
+    [DateDeactivated],
+    [UserMaximumModulePermission],
+    [OnlyAvailableOnBusinessModel],
+    [Lock]
+) VALUES (
+    12,
+    'PURCHASE_ORDER_REPORT_NAME',
+    'PURCHASE_ORDER_REPORT_DESC',
+    'PurchaseOrderReport',
+    0,
+    'Report_Purchase_Orders',
+    1,
+    2,
+    1,
+    0,
+    GETDATE(),
+    '0001-01-01 00:00:00.0000000',
+    '0001-01-01 00:00:00.0000000',
+    '0001-01-01 00:00:00.0000000',
+    3,
+    NULL,
+    1
+);
 ");        }
 
         /// <inheritdoc />
